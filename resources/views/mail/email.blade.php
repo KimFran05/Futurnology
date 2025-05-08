8d8312703a3748689a6e8d5bf5624dbb1107cfb5
Francesca Corsetti <cfrancescaanastasia@gmail.com>
2025-05-08 14:29:43 +0300

    persoane

--- resources/views/mail/email.blade.php
+++ resources/views/mail/email.blade.php
@@ -1,5 +1,5 @@
 Bună ziua, {{ $nume }} {{ $prenume }}! <br>
-Rezervarea dumneavoastră de pe data de {{ $data }}, la ora {{ $ora }}, a fost înregistrată cu succes. <br>
+Rezervarea dumneavoastră pentru {{ $numar_persoane }} persoane, de pe data de {{ $data }}, la ora {{ $ora }}, a fost înregistrată cu succes. <br>
 Vă mulțumim și vă așteptăm cu drag! <br><br>
 
 Futurnology
